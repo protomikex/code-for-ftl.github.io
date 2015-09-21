7456a4472ce9d5b182118731395f52f359827d70
Cristina Solana <cs@nightshiftcreative.com>
2015-09-21 19:15:48 -0400

    small screen fixes, projects page improvements

--- js/controllers/projectsCtrl.js
+++ js/controllers/projectsCtrl.js
@@ -7,6 +7,16 @@
         var model = this;
 
         cfaApi.getProjects(function(data) {
+            model.languageOptions = [
+                {'key': 'css', 'value': 'CSS'},
+                {'key': 'groovy', 'value': 'Groovy'},
+                {'key': 'html', 'value': 'HTML'},
+                {'key': 'java', 'value': 'Java'},
+                {'key': 'javascript', 'value': 'JavaScript'},
+                {'key': 'php', 'value': 'PHP'},
+                {'key': 'python', 'value': 'Python'},
+                {'key': 'ruby', 'value': 'Ruby'}
+            ];
             model.projects = data;
         });
     }
