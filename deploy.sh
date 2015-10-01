cdabb0284d4bd5f4de6dada37852ff2670eeacdc
Cristina Solana <cs@nightshiftcreative.com>
2015-10-01 08:42:22 -0700

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo 'removing existing gh-pages branch...'
+git branch -D gh-pages
+
+echo 'creating gh-pages branch...'
+git checkout --orphan gh-pages
+
+echo 'compiling latest...'
+harp compile
+
+echo 'removing unnecessary files...'
+find . -maxdepth 1 ! -name 'www' ! -name '.*' | xargs rm -rf
+
+echo 'moving build files to root...'
+mv * ../
+
+echo 'commiting build...'
+git add -A; git commit - "latest build"
+
