a160720151a9c7e6359d25dee720dfc83df05535
Cristina Solana <cs@nightshiftcreative.com>
2015-11-16 20:11:02 -0500

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo 'removing existing gh-pages branch...'
-git branch -D gh-pages
+git push origin :master; git branch -D master
 
 echo 'creating gh-pages branch...'
-git checkout --orphan gh-pages
+git checkout --orphan master
 
 echo 'compiling latest...'
 harp compile
@@ -13,8 +13,13 @@ echo 'removing unnecessary files...'
 find . -maxdepth 1 ! -name 'www' ! -name '.*' | xargs rm -rf
 
 echo 'moving build files to root...'
-rm -rf www/www; mv www/* .
+rm -rf www/www; mv www/* .; rm -rf www .sass-cache .idea .gitignore config.rb
 
-echo 'commiting build...'
+echo 'install npm modules...'
+npm install
+
+echo 'committing build...'
 git add -A; git commit -m "latest build"
 
+echo 'pushing to master...'
+git push origin master
