d0a232f6684e97cf21b39bc71f86882ec2f5a1d3
protomikex <protomikex@gmail.com>
2017-04-04 22:11:22 -0400

    Update cfaApi.js

--- js/services/cfaApi.js
+++ js/services/cfaApi.js
@@ -3,14 +3,14 @@
 
     cfmIssuesApp.factory('cfaApi', function($http) {
         var brigadeName = 'CodeForFtL',
-            apiUrl = 'http://api.github.com/search/';
+            apiUrl = 'https://api.github.com/search/';
 
         function getSearch(pageCount, pageNumber, type) {
             var currentPage = pageNumber || 1;
             var url = apiUrl + type + '?per_page=' + pageCount + '&page=' + currentPage + '&q=user:' + brigadeName,
                 options = {
                     headers: {
-                        'User-Agent': 'node.js'
+               //         'User-Agent': 'node.js'
                     }
                 };
 
